f636cfd93bbd31940b8b9e6dc02238305acffc83
Grayson Carroll <carrollgt91@gmail.com>
2015-01-22 12:14:27 -0600

    Removes autocomplete

--- customizations/setup-clojure.el
+++ customizations/setup-clojure.el
@@ -68,16 +68,6 @@
     (cider-interactive-eval (format "(println '(def server (%s/start))) (println 'server)" ns))
     (cider-interactive-eval (format "(def server (%s/start)) (println server)" ns))))
 
-;; Adds autocomplete for cider
-(require 'ac-cider)
-(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
-(add-hook 'cider-mode-hook 'ac-cider-setup)
-(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
-(eval-after-load "auto-complete"
-  '(progn
-     (add-to-list 'ac-modes 'cider-mode)
-     (add-to-list 'ac-modes 'cider-repl-mode)))
-
 (defun cider-refresh ()
   (interactive)
   (cider-interactive-eval (format "(user/reset)")))
